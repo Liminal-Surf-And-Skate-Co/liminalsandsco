069cad09c017aeb796165f8bbc9327e50c9eb7db
Liminal Surf & Skate Co <samuelashleyrosin@gmail.com>
2026-06-25 12:16:32 +0800

    Update vite.config.ts
    Some edits?

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tanstackStart(), netlify(), tailwindcss(), tsConfigPaths()],
+  plugins: [
+    tanstackStart(), 
+    nitro(), 
+    tailwindcss(), 
+    tsConfigPaths()
+  ],
 });
